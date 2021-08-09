dc9475ab97701087cc8ee555bbf2bb944d507f84
yudi <hy-tomioka@github.com>
2021-08-09 14:18:40 -0300

    criacao do projeto

---
+++ src/test/java/br/com/zupacademy/yudi/mercadolivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zupacademy.yudi.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
